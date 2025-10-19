4d2b90dcd59cf9a80c092007d6eaf2996d798f1d
Sara Arévalo <20255938@esen.edu.sv>
2025-10-19 17:04:34 -0600

    Add files via upload

---
+++ Clase 04 - 01.py
@@ -0,0 +1,33 @@
+usuario = "Sara"
+cantidad_alumnos = 79
+media_edad = 18.231234
+monto_hope = 123456.7801
+inversion_evento = -98765.74857
+
+"""print(type(cantidad_alumnos))
+print(type(media_edad))
+
+print(type(cantidad_alumnos) is int)
+print(type(media_edad) is int)"""
+
+"""
+print ("el usuario es", usuario, "y tiene", cantidad_alumnos, "pajaritos en su aula")
+print ("y la edad promedio es de", media_edad)
+
+print(f"El usuario es {usuario}")
+print(f"y en su aula con {cantidad_alumnos - 4} pajaritos")
+print(f"con edad promedio de {media_edad:.1f} años")
+print(f"colectaron {monto_hope:,.2f} como un donativo.")
+print(f"y la totalidad de gastos fue de ${abs(inversion_evento):,.2f} dolares")
+
+print(type(usuario) is str)
+
+esta_lloviendo = False
+print(type(esta_lloviendo) is not bool)
+print(type(monto_hope)is not bool)
+"""
+
+nombre = "Alvin"
+apellido = "Portillo"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
---
+++ Clase 04 - 03.py
@@ -0,0 +1,21 @@
+"""
+ItemIngresar = input("Ingrese el item: ")
+print (f"El item ingresado es {ItemIngresar}")
+"""
+
+my_string = "ABCDECCC" #string o cadena de caracteres
+
+"""letra = int(input("Indique el índice de la letra a mostrar (del 0 al 4): "))
+print(my_string[letra])"""
+
+#print(my_string[1:5:2]) para subsetting por slices
+
+#print ("H" in my_string)
+#print ("O" not in my_string)
+
+#print ("A"*8)
+
+#print(min(my_string))
+#print(max(my_string))
+
+print (my_string.count("C"))
